1b2e1ce3efbfe541b58b7924ce041903911b6e45
Uzi-desing <uzielcares@outlook.com>
2026-08-20 21:15:51 -0300

    feat(informes): crear el template para crear informes y añadir su url

--- src/apps/informes/urls.py
+++ src/apps/informes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('informes/nuevo/', views.crear_informe_view, name='crear_informe'),
 ]
