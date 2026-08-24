07a67376eb096b424916aa290697da5275e31eb5
Uzi-desing <uzielcares@outlook.com>
2026-08-24 18:55:16 -0300

    feat(informes): crear el template para la vista registrar_piezas - Añadir formularios dinamicos, el usuario añade o elimina los formularios que necesita - Incluir la nueva ruta de registrar_piezas al path de urls.py

--- src/apps/informes/urls.py
+++ src/apps/informes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('informes/nuevo/', views.crear_informe_view, name='crear_informe'),
+    path('registrar/<uuid:uuid>/piezas/', views.registrar_piezas, name='registrar_piezas'),
 ]
