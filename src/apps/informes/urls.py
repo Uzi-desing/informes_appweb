0826b0f767995f1e8f9314f91a939bbbd00515d0
Uzi-desing <uzielcares@outlook.com>
2026-08-25 12:29:34 -0300

    feat(informes): agregar cancelar informe al urls.py y su respectivo template

--- src/apps/informes/urls.py
+++ src/apps/informes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('informes/nuevo/', views.crear_informe_view, name='crear_informe'),
-    path('registrar/<uuid:uuid>/piezas/', views.registrar_piezas, name='registrar_piezas'),
+    path('registrar/<uuid:uuid>/piezas/', views.registrar_piezas_view, name='registrar_piezas'),
+    path('registrar/<uuid:uuid>/cancelar/', views.cancelar_informe_view, name='cancelar_informe'),
 ]
