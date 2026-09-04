d3056636597c127775ea044344c002bd4d2a9e78
Uzi-desing <uzielcares@outlook.com>
2026-09-04 19:16:52 -0300

    feat(admin): configurar nueva ruta para el sitio admin

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminsite/', admin.site.urls),
     path('', include('apps.usuarios.urls')),
     path('', include('apps.informes.urls')),
 ]
